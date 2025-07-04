e58ba1eb9b2955d5205b42621ab37c34dcf61b93
KugaPolina <142220326+KugaPolina@users.noreply.github.com>
2025-07-04 12:29:48 +0300

    Add files via upload

--- app.R
+++ app.R
@@ -10,44 +10,23 @@ prediction_model <- readRDS('model_glm_Hb.RDS')
 i18n <- Translator$new(translation_json_path = "syn.json")
 i18n$set_translation_language("ru")
 
-# Diagnosis + Organomegaly + 
-#   BMI + Age + Temperature + V_weight
-
-
-# r <- i18n$t("МДС, ЮММЛ")
-# z <- i18n$t("Иное")
-
-
 ui <- fluidPage(
   shiny.i18n::usei18n(i18n),
   div(style = "absolute: right;
     text-align: center;
     width: 70px",
-    selectInput('selected_language',
-                i18n$t(""),
-                choices = i18n$get_languages(),
-                selected = i18n$get_key_translation())
+      selectInput('selected_language',
+                  i18n$t(""),
+                  choices = i18n$get_languages(),
+                  selected = i18n$get_key_translation())
   ),
   
-  # navbarPage("MyApp", 
-  #            theme = shinytheme("flatly")),
-  # 
-  # dashboardHeader(
-  #   title = "MY TITLE",
-  #   tags$li(class = "dropdown",     
-  #           radioButtons(inputId = "language",
-  #                        label = "",
-  #                        choices = c("日文" = "cn", "English" = "en"),
-  #                        selected = "jp")
-  #   )
-  # ),
-  
-
-  headerPanel(i18n$t('Прогностический калькулятор дозы эритроцитной взвеси для детей в онкогематологии')),
+  headerPanel(i18n$t('Прогностический калькулятор дозы эритроцитной взвеси для детей с онкологическими и гематологическими заболеваниями')),
   
   usei18n(i18n),
   sidebarPanel(
     numericInput('Age', i18n$t('Возраст, мес.'), 12),
+    numericInput('Weight', i18n$t('Вес, кг'), 10),  # Добавлено поле для ввода веса
     
     selectInput('Diagnosis', i18n$t('Заболевание'), 
                 choices = c("МДС, ЮММЛ", "Иное"), 
@@ -67,15 +46,8 @@ ui <- fluidPage(
     h4(textOutput(outputId = "textRes4")),  
     p(textOutput(outputId = "textRes2")),  
     p(textOutput(outputId = "textRes3")),
+    h3(textOutput(outputId = "textRes5"))  # Добавлен вывод рассчитанной дозы
   ),
-  
-  # div(style = "float: left;
-  #   bottom:0",
-  # 
-  # i18n$t('Куга П.С., клиника «НИИ Детской онкологии, гематологии и трансплантологии им.Р.М.Горбачевой» ФГБОУ ВО ПСПбГМУ им.И.П.Павлова  МЗ РФ. \n E-mail: polusha-1981@yandex.ru')
-  # 
-  # ),
-  
   
   tags$footer(style = "float: center;
                        text-align: center;
@@ -90,35 +62,17 @@ ui <- fluidPage(
                            <p href='doctorpolinakuga@gmail.com'>E-mail: doctorpolinakuga@gmail.com</p>
                            </div>
                            <!-- Copyright -->
-
                            </footer>
                            <!-- Footer -->"))
-  
 )
-  
-
-
-
-
-
-
-
-
 
 server <- function(input, output, session) {
-  
-  
-  
-# Combine the selected variables into a new data frame
-  
   i18n_r <- reactive({
     i18n
   })
-
+  
   observeEvent(input$selected_language, {
-    # This print is just for demonstration
     print(paste("Language change!", input$selected_language))
-    # Here is where we update language in session
     shiny.i18n::update_lang(input$selected_language)
   })
   
@@ -130,13 +84,6 @@ server <- function(input, output, session) {
     i18n
   })
   
-  # observeEvent(input$change, {
-  #   lang <- ifelse(as.numeric(input$change) %% 2, "ru", "en")
-  #   shiny.i18n::update_lang(session, lang)
-  #   i18n_r()$set_translation_language(lang)
-  # })
-  
-
   observe({
     updateSelectInput(session, "Diagnosis", label = asda()$t("Заболевание"),
                       choices = asda()$t(c("МДС, ЮММЛ", "Иное")))
@@ -148,18 +95,6 @@ server <- function(input, output, session) {
                       choices = asda()$t(c("Фебрильная", "Иное")))
   })
   
-  
-  
-
-  
-  
-  
-  # observeEvent(input$change, {
-  #   lang <- ifelse(as.numeric(input$change) %% 2, "ru", "en")
-  #   shiny.i18n::update_lang(session, lang)
-  #   i18n_r()$set_translation_language(lang)
-  # })
-  
   selectedData <- reactive({
     data.frame(Age = input$Age/12,
                Diagnosis = 1*(input$Diagnosis == asda()$t("МДС, ЮММЛ")),
@@ -167,26 +102,22 @@ server <- function(input, output, session) {
                BMI = 1*(input$BMI == asda()$t("Избыток массы тела")),
                Temperature = 1*(input$Temperature == asda()$t("Фебрильная")),
                V_weight =5:30
-               )
+    )
   })
   
   prediction_results <- reactive({
     tmp <- predict(prediction_model, 
-            newdata = selectedData(),
-            interval = "prediction", 
-            level = 0.9) %>% 
+                   newdata = selectedData(),
+                   interval = "prediction", 
+                   level = 0.9) %>% 
       data.frame(.)
     
     cbind(tmp, selectedData())
-    
   })
   
-  
   output$plot1 <- renderPlot({
-    
-
     ggplot(prediction_results(), aes(x = V_weight, 
-                                 y = fit)) +
+                                     y = fit)) +
       geom_line(color = "black") +
       geom_line(mapping = aes(y = lwr), color = "red", linetype = "dashed") +
       geom_line(mapping = aes(y = upr), color = "red", linetype = "dashed") +
@@ -214,7 +145,6 @@ server <- function(input, output, session) {
                   aes( ymin=30, ymax = upr), 
                   fill="red", 
                   alpha=0.3) +
-    
       xlab(asda()$t('Доза эритроцитной взвеси, мл/кг ')) +
       ylab(asda()$t('Диапазон вероятного прироста Hb, г/л ')) +
       scale_x_continuous(breaks = seq(5,30,1), limits = c(5,15)) +
@@ -228,35 +158,44 @@ server <- function(input, output, session) {
             legend.text = element_text(size = 14),
             legend.title = element_text(size=14))   
   })
-
+  
   output$textRes1 <- renderText({
-   paste0( 
-          as.character(round(approx(prediction_results()$lwr, prediction_results()$V_weight, 10)$y, 1)),
-          " - ", as.character(round(approx(prediction_results()$upr, prediction_results()$V_weight, 30)$y, 1)), 
-          asda()$t(" мл/кг"))  
+    paste0( 
+      as.character(round(approx(prediction_results()$lwr, prediction_results()$V_weight, 10)$y, 1)),
+      " - ", as.character(round(approx(prediction_results()$upr, prediction_results()$V_weight, 30)$y, 1)), 
+      asda()$t(" мл/кг"))  
   })  
   
+  # Добавлен расчет дозы с учетом веса
+  output$textRes5 <- renderText({
+    min_dose <- round(approx(prediction_results()$lwr, prediction_results()$V_weight, 10)$y, 1)
+    max_dose <- round(approx(prediction_results()$upr, prediction_results()$V_weight, 30)$y, 1)
+    weight <- input$Weight
+    
+    min_volume <- round(min_dose * weight, 1)
+    max_volume <- round(max_dose * weight, 1)
+    
+    paste0(asda()$t("Рекомендуемый объем: "), 
+           min_volume, " - ", max_volume, 
+           asda()$t(" мл (для веса "), weight, " кг)")
+    
+ })
 
-  
-  output$textRes4 <- renderText({
-    paste0(asda()$t("Эффективная и безопасная доза эритроцитной взвеси в текущих клинических условиях, с вероятностью более 90%"))
-  })  
-  
-  
-  output$textRes2 <- renderText({
-    paste0(asda()$t("* В дозе менее "), 
-           as.character(round(approx(prediction_results()$lwr, prediction_results()$V_weight, 10)$y, 1)),
-           asda()$t(" мл/кг - трансфузия может быть неэффективна (прирост Hb < 10г/л), вероятнее потребуется повторная трансфузия для достижения клинического эффекта"))  
-  })  
-  
-  output$textRes3 <- renderText({
-    paste0(asda()$t("* В дозе более "), 
-           as.character(round(approx(prediction_results()$upr, prediction_results()$V_weight, 30)$y, 1)),
-           asda()$t(" мл/кг - возрастает вероятность неконтролируемого подъема уровня Hb > 30г/л, что может повысить риск развития осложнений, связанных с резким подъемом гематокрита"))  
-  })  
-  
-  
-  
+output$textRes4 <- renderText({
+  paste0(asda()$t("Эффективная и безопасная доза эритроцитной взвеси в текущих клинических условиях, с вероятностью более 90%"))
+})  
+
+output$textRes2 <- renderText({
+  paste0(asda()$t("* В дозе менее "), 
+         as.character(round(approx(prediction_results()$lwr, prediction_results()$V_weight, 10)$y, 1)),
+         asda()$t(" мл/кг - трансфузия может быть неэффективна (прирост Hb < 10г/л), вероятнее потребуется повторная трансфузия для достижения клинического эффекта"))  
+})  
+
+output$textRes3 <- renderText({
+  paste0(asda()$t("* В дозе более "), 
+         as.character(round(approx(prediction_results()$upr, prediction_results()$V_weight, 30)$y, 1)),
+         asda()$t(" мл/кг - возрастает вероятность неконтролируемого подъема уровня Hb > 30г/л, что может повысить риск развития осложнений, связанных с резким подъемом гематокрита"))  
+})  
 }
 
 shinyApp(ui, server)
